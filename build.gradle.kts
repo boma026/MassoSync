10c1e4c13868b2190ee094359da160afb3c99230
boma026 <arthurboma@yahoo.com.br>
2026-09-11 11:20:21 -0300

    fix: configura jvmTarget do detekt para compatibilidade do ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,4 +7,15 @@ plugins {
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinJvm) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.6.1"
+    id("io.gitlab.arturbosch.detekt") version "1.23.3"
+}
+
+allprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+
+    tasks.withType<io.gitlab.arturbosch.detekt.Detekt>().configureEach {
+        jvmTarget = "11"
+    }
 }
